84c34a812d24a3e586e5c245c36b2e79c971cc5c
Miguel Delgado <105407860+MigueDeep@users.noreply.github.com>
2024-11-27 16:26:39 -0600

    Add files via upload

---
+++ src/views/Home.vue
@@ -0,0 +1,14 @@
+<!-- src/views/Home.vue -->
+<template>
+    <div>
+      <h1>Home</h1>
+      <p>Bienvenido a tu cambio deploy por jenkins jeje olvide poner el build.</p>
+    </div>
+  </template>
+  
+  <script>
+  export default {
+    name: 'HomePage'
+  }
+  </script>
+  
